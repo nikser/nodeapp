d60c58d537cba39e7e06b23978eb95efb7e43c76
Maksym Koval <srcmaxim@gmail.com>
2018-12-08 16:27:58 +0200

    Added Docker-compose dev and prod builds.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-	res.json({'msg': 'hello word'})
+	res.json({'timestamp': new Date().toJSON()})
 });
 
 var port = process.env.port || 3000;
