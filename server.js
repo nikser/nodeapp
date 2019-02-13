48a0c9230d9ed9fa7305070c1d3bda780c573ab5
koval.m <koval.m@dbbest.com>
2019-02-13 17:13:46 +0200

    Added state to application

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
-var express = require('express');
+/* Dependencies */
+const express = require("express");
+const bodyParser = require("body-parser");
+
+
+/* Configuration */
 var app = express();
 
-app.get('/', function(req, res) {
-	res.json({'timestamp': new Date().toJSON()})
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+
+
+/* Application */
+let data = null;
+
+app.post("/", function (req, res) {
+	data = req.body;
+	console.log("Reseived data: " + JSON.stringify(req.body));
+	res.json({'data': data});
 });
 
+app.get("/", function(req, res) {
+	res.json({"data": data});
+});
+
+/* Running */
 var port = process.env.port || 3000;
 app.listen(port, function() {
-	console.log('curl http://localhost:' + port);
+	console.log("Application started: curl http://localhost:" + port);
 });
