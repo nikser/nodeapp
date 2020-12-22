519e61a61a5a225944233077d393074c4cd1d032
Nikolay Efimov <nefimov@plesk.com>
2020-12-22 13:49:21 +0700

    Add metrics

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,11 @@ FROM node:8.14.0
 RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
 
-COPY ./server.js ./package.json ./persons.js ./data.js /usr/src/app/
+COPY ./package.json /usr/src/app/
 RUN npm install
 
 EXPOSE 3000
 
 CMD [ "npm", "start" ]
+
+COPY ./server.js ./persons.js ./data.js /usr/src/app/
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 /* JS features */
-Array.prototype.flatMap = function(f) {
-  return this.map(f).reduce((x,y) => x.concat(y), [])
+Array.prototype.flatMap = function (f) {
+	return this.map(f).reduce((x, y) => x.concat(y), [])
 }
 
 /* Dependencies */
@@ -12,21 +12,35 @@ const Persons = require('./persons');
 var app = express();
 
 app.use(bodyParser.urlencoded({
-    extended: true
+	extended: true
 }));
 
 app.use(bodyParser.json());
 
 
 /* Application */
-app.get("/persons", function(req, res) {
+app.get("/", function (req, res) {
+	res.send('Node app - OK');
+});
+
+app.get("/persons", function (req, res) {
 	const name = req.query.name;
 	const person = Persons.findPersonByName(name);
 	res.json(person);
 });
 
+/* Metrics */
+app.get("/metrics", function (req, res) {
+	res
+		.type('text/plain')
+		.send(`# HELP process_virtual_memory_bytes Virtual memory size in bytes.
+# TYPE process_virtual_memory_bytes gauge
+my_custom_app_metric 133
+`);
+});
+
 /* Running */
 var port = process.env.port || 3000;
-app.listen(port, function() {
+app.listen(port, function () {
 	console.log("Application started: curl http://localhost:" + port);
 });
